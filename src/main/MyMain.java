24168e825c10005532476a23d3a63d1d9288f736
PRAVIN <pravin.singh_cs19@gla.ac.in>
2021-04-13 15:40:27 +0530

    only iplementation left

--- src/main/MyMain.java
+++ src/main/MyMain.java
@@ -1,7 +1,20 @@
 package main;
+import implementation.MyBinarySearchTree;
 
 public class MyMain {
     public static void main(String[] args) {
+        MyBinarySearchTree bst = new MyBinarySearchTree();
+        bst.insert(50);
+        bst.insert(25);
+        bst.insert(75);
+        bst.insert(10);
+        bst.insert(35);
+        bst.insert(40);
+        System.out.println(bst.height(bst.getRoot()));
+        System.out.println();
+        bst.delete(50);
+        System.out.println();
+        System.out.println();
         //create binary search tree
         // and perform insert, search, traversal, delete
     }
